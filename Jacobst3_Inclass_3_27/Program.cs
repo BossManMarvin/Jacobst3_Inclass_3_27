bedaffb4d0e0ce5009474ce9940c46d16254825d
Miles Jacobson <jacobst3@mail.uc.edu>
2025-03-27 11:44:30 -0400

    Add project files.

---
+++ Jacobst3_Inclass_3_27/Program.cs
@@ -0,0 +1,40 @@
+﻿/*File Name: {required}
+
+*Name: { required}
+*email:  { required}
+*Assignment Number: Assignment nn  { required}
+*Due Date: { required}
+*Course #/Section:   {required}
+* Semester/Year:   { required}
+*Brief Description of the assignment:  { required}
+
+*Brief Description of what this module does. {Do not copy/paste from a previous assignment. Put some thought into this. required}
+*Citations: { Link to specific pages. "StackOverflow" or "w3Schools" is not sufficient. }
+*Anything else that's relevant:
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Jacobst3_Inclass_3_27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+    internal class Test
+    {
+        static void TestMethod()
+        {
+            Console.WriteLine("This counts");
+        }
+    }
+
+}
